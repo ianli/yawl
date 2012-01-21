229a28b28daa156454ba10590487891c1ab4b999
Ian Li <ianl@argentina.hcii.cmu.edu.home>
2012-01-21 18:52:37 -0500

    Added Fork me on GitHub banner

--- _layouts/default.php
+++ _layouts/default.php
@@ -26,7 +26,8 @@
       <div class="hero-unit">
         <h1>YAWL <span>&amp; Others!</span></h1>
         <p>
-          A demo of YAWL, a very simple blogging platform in PHP.
+          A demo of <a href="http://github.com/ianli/yawl/">YAWL</a>,
+          a very simple blogging platform in PHP.
         </p>
       </div>
       
@@ -41,6 +42,8 @@
     </div>
   </div>
 </div>
+
+<a href="http://github.com/ianli/yawl/"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://a248.e.akamai.net/assets.github.com/img/71eeaab9d563c2b3c590319b398dd35683265e85/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub"></a>
 
 </body>
 </html>
