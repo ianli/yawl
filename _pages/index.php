f3775262fe7c8eb8f6bd57dfd8776cf0c7fa692e
Ian Li <ianarli@gmail.com>
2012-01-22 13:23:29 -0500

    Added index.php which displays posts in the site.

--- _pages/index.php
+++ _pages/index.php
@@ -1,1 +1,20 @@
-This is the index. Testing
+<?php
+
+  $front_matter = array(
+    'title' => 'YAWL &amp; Others!'
+  );
+  
+  $content = function () {
+    global $raft;
+
+    ?>
+      <?php foreach ($raft['site.posts'] as $post) { ?>
+        <p>
+          <?= strftime('%Y %b %d', $post['date']) ?>
+          <a href="<?= $post['permalink'] ?>"><?= $post['title'] ?></a>
+        </p>
+      <?php } ?>
+    <?php
+    
+  }
+?>
