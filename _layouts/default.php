7944f3e0cd3fd27a8cd09948d92c88cdb9e5f648
Ian Li <ianl@argentina.hcii.cmu.edu.home>
2012-01-22 12:24:15 -0500

    Changed references to page variables

--- _layouts/default.php
+++ _layouts/default.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html> 
 <head>
-	<title><?= raft("title") ?></title>
+	<title><?= raft("page.title") ?></title>
 	
 	<!-- UTF-8 (Unicode) encoding -->
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
@@ -15,8 +15,6 @@
 	<link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css" />
 	
 	<link rel="stylesheet" href="css/default.css" type="text/css" />
-
-	<?= raft("head") ?>
 </head>
 <body>
 
@@ -31,8 +29,10 @@
         </p>
       </div>
       
-      <div id="content">
-        <?= raft("content") ?>
+      <div class="row">
+        <div class="offset1 span8" id="content">
+          <?= raft("page.content") ?>
+        </div>
       </div>
       
       <footer>
@@ -43,6 +43,7 @@
   </div>
 </div>
 
+<!-- GitHub Ribbon -->
 <a href="http://github.com/ianli/yawl/"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://a248.e.akamai.net/assets.github.com/img/71eeaab9d563c2b3c590319b398dd35683265e85/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub"></a>
 
 </body>
